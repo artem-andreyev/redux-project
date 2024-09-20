ee36e3f227fad6d93e26999509032cb59389c181
Artem <artem.andreyev212@gmail.com>
2024-09-20 22:55:53 +0300

    Working with redux with counter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {legacy_createStore as createStore} from 'redux'
+
+const initialState = 0;
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case "INC":
+            return state + 1;
+        case "DEC":
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+const update = () => {
+  document.getElementById("counter").textContent = store.getState();
+}
+
+store.subscribe(update);
+
+document.getElementById("inc").addEventListener("click", () => {
+    store.dispatch({type: "INC"});
+});
+
+document.getElementById("dec").addEventListener("click", () => {
+    store.dispatch({type: "DEC"});
+});
+
+// let state = reducer(initialState, {type: "INC"});
+// state = reducer(state, {type: "INC"});
+// state = reducer(state, {type: "INC"});
+// state = reducer(state, {type: "INC"});
+// console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
