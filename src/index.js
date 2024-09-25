ded36e73a37c9e461d5d47e38b927b752e41db3c
Artem <artem.andreyev212@gmail.com>
2024-09-25 22:27:00 +0300

    Adding React in project

--- src/index.js
+++ src/index.js
@@ -4,39 +4,29 @@ import {legacy_createStore as createStore, bindActionCreators} from 'redux'
 import reducer from './reducer';
 import * as actions from "./actions";
 
+import App from './components/App';
 
 const store = createStore(reducer);
 
 const {dispatch, subscribe, getState} = store;
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
 const update = () => {
-    document.getElementById("counter").textContent = getState().value;
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+          {/* <Counter
+            counter={getState().value}
+            inc={inc}
+            dec={dec}
+            rnd={() => {
+                const value = Math.floor(Math.random() * 10);
+                rnd(value);
+            }}
+          /> */}
+          <App/>
+      </React.StrictMode>
+    );
 }
-
+update();
 subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//     dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById("inc").addEventListener("click", inc);
-
-document.getElementById("dec").addEventListener("click", dec);
-
-document.getElementById("rnd").addEventListener("click", () => {
-    const value = Math.floor(Math.random() * 10);
-    rnd(value);
-});
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <>
-
-    </>
-  </React.StrictMode>
-);
