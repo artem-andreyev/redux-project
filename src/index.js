a7f9917c51c924a6d01044ff085cd668b9591af5
Artem <artem.andreyev212@gmail.com>
2024-09-26 22:58:41 +0300

    Added Provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,31 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {legacy_createStore as createStore, bindActionCreators} from 'redux'
 import reducer from './reducer';
-import * as actions from "./actions";
+import {Provider} from "react-redux";
 
 import App from './components/App';
 
 const store = createStore(reducer);
 
-const {dispatch, subscribe, getState} = store;
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-
-const update = () => {
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-      <React.StrictMode>
-          {/* <Counter
-            counter={getState().value}
-            inc={inc}
-            dec={dec}
-            rnd={() => {
-                const value = Math.floor(Math.random() * 10);
-                rnd(value);
-            }}
-          /> */}
-          <App/>
-      </React.StrictMode>
-    );
-}
-update();
-subscribe(update);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
+);
