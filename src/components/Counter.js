88ad7b0a1caefdac77e15125b194162ec25daf95
Artem <artem.andreyev212@gmail.com>
2024-09-27 23:54:26 +0300

    Linked React with Redux using connect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,40 @@
+import { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const Counter = ({counter, inc, dec, rnd}) => {
-    return (
-        <div className="jumbotron">
+// const Counter = ({counter, inc, dec, rnd}) => {
+//     return (
+//         <div className="jumbotron">
+//             <h1>{counter}</h1>
+//             <button onClick={dec} className="btn btn-primary">DEC</button>
+//             <button onClick={inc} className="btn btn-primary">INC</button>
+//             <button onClick={rnd} className="btn btn-primary">RND</button>
+//         </div>
+//     )
+// }
+
+class Counter extends Component {
+    render() {
+        const {counter, inc, dec, rnd} = this.props;
+        return (
+            <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
-        </div>
-    )
+             <button onClick={dec} className="btn btn-primary">DEC</button>
+             <button onClick={inc} className="btn btn-primary">INC</button>
+             <button onClick={rnd} className="btn btn-primary">RND</button>
+         </div>
+        )
+    }
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        counter: state.value
+    }
+}
+
+// const mapDispatchToProps = (dispatch) => {
+//     return bindActionCreators(actions, dispatch); // inc, dec, rnd
+// }
+
+export default connect(mapStateToProps, actions)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {legacy_createStore as createStore, bindActionCreators} from 'redux'
+import {legacy_createStore as createStore} from 'redux'
 import reducer from './reducer';
 import {Provider} from "react-redux";
 
